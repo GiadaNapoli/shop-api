97d7261431e1dfb8bfda3ff4b513547b212be935
GiadaNapoli <giadanapoli94@gmail.com>
2024-05-13 13:58:33 +0200

    Implement password encryption functions using bcrypt

--- dist/models/user.model.js
+++ dist/models/user.model.js
@@ -1,10 +1,20 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
+const common_functions_1 = require("../utility/common.functions");
 const userSchema = new mongoose_1.default.Schema({
     name: {
         type: String,
@@ -23,4 +33,16 @@ const userSchema = new mongoose_1.default.Schema({
         required: true,
     },
 }, { timestamps: true });
+userSchema.pre("save", function (next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = this;
+        try {
+            user.password = yield (0, common_functions_1.hashPassword)(user.password);
+            next();
+        }
+        catch (error) {
+            next();
+        }
+    });
+});
 exports.User = mongoose_1.default.model("User", userSchema);
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { ZUserSchema } from "../validators/user.validator";
+import { hashPassword } from "../utility/common.functions";
 
 const userSchema = new mongoose.Schema<ZUserSchema>(
 	{
@@ -23,4 +24,13 @@ const userSchema = new mongoose.Schema<ZUserSchema>(
 	{ timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+	const user = this;
+	try {
+		user.password = await hashPassword(user.password);
+		next();
+	} catch (error) {
+		next();
+	}
+});
 export const User = mongoose.model("User", userSchema);
